41c27e7dfe7a5e0bbc44e2e4b5e20950d6409afe
juan ramirez <ramirezlozanojuanjose@gmail.com>
2026-02-22 08:18:02 -0400

    ultimos cambios antes de migrar a fedora

--- frontend/venezuelan-interactive-story/src/components/header/index.js
+++
@@ -1,3 +0,0 @@
-// Este archivo exporta todos los componentes del encabezado.
-export { default as AppHeader } from './AppHeader';
-export { default as AppHeaderDropdown } from './AppHeaderDropdown';
